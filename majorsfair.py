7021225498c92d637e85ad3e2d32bfa1159b1efd
Kathy <kim.kathleen97@gmail.com>
2020-10-29 18:41:17 -0400

    Sorting fixed

--- majorsfair.py
+++ majorsfair.py
@@ -15,16 +15,16 @@ def similar(a, b):
     return SequenceMatcher(None, a, b).ratio()
 
 
-def find_best_match(majors):
+def find_best_match(majors, category_list, category_names, bad_words):
+    
     matches = {'Creative': [], 'Life': [], 'Leadership': [],
-               'Service': [], 'Technology': [], 'Culture': [], 'Nature': []}
-
+             'Service': [], 'Technology': [], 'Culture': [], 'Nature': []}
     # Find the best matching category
-    best_match, best_ratio = None, 0
     for item in majors:  # For each major in the excel row
         item = item.strip()
         if item in bad_words:
             break
+        best_match, best_ratio = None, 0
         for category_name, majors_list in zip(category_names, category_list):
             for true_major in majors_list:
                 ratio = similar(item, true_major)
@@ -32,6 +32,10 @@ def find_best_match(majors):
                     best_match = category_name
                     best_ratio = ratio
         matches[best_match].append(item)
+
+        # if item.casefold() == "Exploratory Center":
+        #     matches[best_match].append(item)
+
     return matches
 
 
@@ -40,8 +44,8 @@ csv_path = "majors_categories.csv"
 xl_file = "masterlist.xlsx"
 df = pd.read_csv(csv_path, sep=',')
 
-creative_list, leadership_list, life_list, service_list = [], [], [], []
-tech_list, culture_list, nature_list = [], [], []
+# creative_list, leadership_list, life_list, service_list = [], [], [], []
+# tech_list, culture_list, nature_list = [], [], []
 
 # append all of the data to lists
 creative_list = list(df["Creative"].dropna())
@@ -51,8 +55,12 @@ service_list = list(df["Service"].dropna())
 tech_list = list(df["Technology"].dropna())
 culture_list = list(df["Culture"].dropna())
 nature_list = list(df["Nature"].dropna())
-all_lists = creative_list + leadership_list + life_list + \
-    service_list + tech_list + culture_list + nature_list
+# all_lists = creative_list + leadership_list + life_list + service_list + tech_list + culture_list + nature_list
+category_list = [creative_list, life_list, leadership_list,
+                    service_list, tech_list, culture_list, nature_list]
+category_names = ['Creative', 'Life', 'Leadership',
+                'Service', 'Technology', 'Culture', 'Nature']
+bad_names = ['nan']
 
 # read the majors fair excel file
 xl = pd.ExcelFile(xl_file)
@@ -69,13 +77,6 @@ organized_dict = {}
 
 responses = xl.parse("Form Responses 1")
 
-placeholder_dict = {}
-category_list = [creative_list, life_list, leadership_list,
-                 service_list, tech_list, culture_list, nature_list]
-category_names = ['Creative', 'Life', 'Leadership',
-                  'Service', 'Technology', 'Culture', 'Nature']
-bad_words = ['nan']
-
 # Get every zoom link, match it to the majors
 for row in responses.iterrows():
     majors = str(row[1][4]).split(",") + str(row[1][5]).split(",")
@@ -83,9 +84,9 @@ for row in responses.iterrows():
     certificates = str(row[1][8]).split(",") + str(row[1][9]).split(",")
 
     # Method that takes in major/minor/category and then returns the best match, then appends value to dictionary key[name]
-    names_to_major_dict[str(row[1][1])] = find_best_match(majors)
-    names_to_minor_dict[str(row[1][1])] = find_best_match(minors)
-    names_to_cat_dict[str(row[1][1])] = find_best_match(certificates)
+    names_to_major_dict[str(row[1][1])] = find_best_match(majors, category_list, category_names, bad_names)
+    names_to_minor_dict[str(row[1][1])] = find_best_match(minors, category_list, category_names, bad_names)
+    names_to_cat_dict[str(row[1][1])] = find_best_match(certificates, category_list, category_names, bad_names)
 
 # go through all the xl sheets and pull the zoom links, match them to the correct person
 for sheet in xl.sheet_names:
@@ -102,14 +103,24 @@ for key in sorted(names_to_major_dict.keys()):
         # for name in category_names:
         #     col = 'Majors: ' + ', '.join(names_to_major_dict[key]) + ', minors: '
         # creative = names_to_major_dict[key]['Creative'] + names_to_minor_dict[key]['Creative']
+        # print(names_to_major_dict[key])
 
-        organized_dict[key + ' - Majors'] = [names_to_zoom_dict[key], *sorted(names_to_major_dict[key].values())]
-        organized_dict[key + ' - Minors'] = ['', *sorted(names_to_minor_dict[key].values())]
-        organized_dict[key + ' - Certificates'] = ['', *sorted(names_to_cat_dict[key].values())]
+        print()
+
+
+        organized_dict[key + ' - Majors'] = [names_to_zoom_dict[key],
+                                             *[sorted(items) for items in names_to_major_dict[key].values()]]
+        organized_dict[key + ' - Minors'] = ['',
+                                             *[sorted(items) for items in names_to_minor_dict[key].values()]]
+        organized_dict[key + ' - Certificates'] = ['',
+                                                   *[sorted(items) for items in names_to_cat_dict[key].values()]]
 
 organized_df = pd.DataFrame.from_dict(organized_dict, orient="index")
-organized_df = organized_df.rename(columns={0:"Zoom Links", 1:"Creative", 2:"Life", 3:"Leadership", 4:"Service", 5:"Technology",6:"Culture", 7:"Nature"})
-organized_df.to_excel(r'./Organized_MasterList.xlsx')
+organized_df = organized_df.rename(columns={0: "Zoom Links", 1: "Creative", 2: "Life", 3:"Leadership", 4:"Service", 5:"Technology",6:"Culture", 7:"Nature"})
+print(organized_df)
+organized_df.to_csv(r'./Organized_MasterList.csv')
+
+#organized_df.to_excel(r'./Organized_MasterList.xlsx')
 
 # for word in major.split('-')[0].strip().split(' '):  # Take all the words before the dash -
 #     if word in bad_words: break  # Skip if bad word
