aea20b341c5f35a2c409eebaa63ebbc3eb44abdd
Kathy <kim.kathleen97@gmail.com>
2020-11-02 12:36:28 -0500

    reorganizing

--- majors-react-app/hello.py
+++ majors-react-app/hello.py
@@ -2,30 +2,29 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
 import majorsfair
 import logging
-from werkzeug.datastructures import ImmutableMultiDict
+#from werkzeug.datastructures import ImmutableMultiDict
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
 app.config['CORS_HEADERS'] = 'Content-Type'
-UPLOAD_FOLDER = "./"
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+# UPLOAD_FOLDER = "./"
+# app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@app.route('/api/upload/', methods=['GET', 'POST'])
-@cross_origin(supports_credentials=True)
-def upload():
-    if(request.method == 'POST'):
-        f = request.form
-        for key in f.keys():
-            for value in f.getlist(key):
-                print (key,":",value)
-        return "Error"
+# @app.route('/api/upload/', methods=['GET', 'POST'])
+# @cross_origin(supports_credentials=True)
+# def upload():
+#     if(request.method == 'POST'):
+#         f = request.form
+#         for key in f.keys():
+#             for value in f.getlist(key):
+#                 print (key,":",value)
+#         return "Error"
 
 
 @app.route('/api/', methods=['GET', 'POST'])
 @cross_origin(supports_credentials=True)
 def api_post():
     if (request.method == 'POST'):
-        print(request.get_json())
         return majorsfair.jsonfiles
     else:
         return "Hello World!"
